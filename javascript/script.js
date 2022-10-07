c576e8972e40dce6def8306ed72e15dfc33d5bf9
LucasCarvalho23 <lucasilva1990@hotmail.com>
2022-10-06 21:36:40 -0300

    JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,120 +1,62 @@
-let library = [
-    'qatar', 'ecuardor', 'senegal', 'netherlands', 'england', 'iran', 'usa', 'wales', 'argentina', 'saudiarabia', 'mexico', 'poland', 'france', 'australia', 'denmark', 'tunisia', 'spain', 'costarica', 'germany', 'japan', 'belgium', 'canada', 'morocco', 'croatia', 'brazil', 'serbia', 'switzerland', 'cameroon', 'portugal', 'ghana', 'uruguay','korea'
-]
-let amount = library.length-1
-let position = Math.round(Math.random() * amount)
-let word = library[position]
-let heightWord = word.length
-let letterBox = []
-let hits 
-let errorMax = 7
-let errors = 0
-let drawing = []
-let gotItRight = false
-let playing = false 
-let letterJ = document.querySelector ("#letterJ-id")
+class Hangman {
 
-function startGame() {
-    playing = true 
-    letterJ.value = ""
-    letterJ.focus()
-    hits = 0
-    errors = 0
-    gotItRight = false 
-    document.querySelector("#container-hangman-letterTyped-id").innerHTML = `Letter typed: `
-    position = Math.round(Math.random() * amount)
-    word = library[position]
-    heightWord = word.length
+    constructor() {
+        this.library = [
+            'qatar', 'ecuardor', 'senegal', 'netherlands', 'england', 'iran', 'usa', 'wales', 'argentina', 'saudiarabia', 'mexico', 'poland', 'france', 'australia', 'denmark', 'tunisia', 'spain', 'costarica', 'germany', 'japan', 'belgium', 'canada', 'morocco', 'croatia', 'brazil', 'serbia', 'switzerland', 'cameroon', 'portugal', 'ghana', 'uruguay','korea'
+        ]
 
-    defineLetter(heightWord)
+        this.totalWords = this.library.length
+        this.randomNumber = Math.round(Math.random() * this.totalWords)
+        this.letterArray = []
 
-    document.querySelector("#container-hangman-messenger-id").innerHTML = ``
+        this.buttonStartId = document.querySelector ("#button-start-id")
+        this.tipLetterButton = document.querySelector ("#tipLetter-button-id")
 
-    drawing[1] = document.querySelector("#head-img-id")
-    drawing[2] = document.querySelector("#body-img-id")
-    drawing[3] = document.querySelector("#right-arm-img-id")
-    drawing[4] = document.querySelector("#left-arm-img-id")
-    drawing[5] = document.querySelector("#right-leg-img-id")
-    drawing[6] = document.querySelector("#left-leg-img-id")
-    document.querySelector("#head-img-id").src = "./images/head.png"
+        this.buttonStartId.addEventListener("click",() => this.startGame())
 
-    for (let count = 1; count < 7; count++) {
-        drawing[count].style.display = "none"
+        // Tip
+        this.tipLetterButton.addEventListener("click",() => this.tipValue())
+        
     }
 
-}
+    startGame() {
+        this.wordChoiced = this.library[this.randomNumber]
+        this.totalCharWordChoiced = this.library[this.randomNumber].length
 
-
-function defineLetter(l) {
-    let obj
-    for (let count = 0; count < 20; count++) {
-        obj.document.querySelector("letter" + count).value = ""
-        obj.document.querySelector("letter" + count).style.display = "none"
+        this.declareCubes()
+        this.insertCubes()
+        
     }
-    for (let count = 0; count < l; count++) {
-        obj.document.querySelector("letter" + count).style.display = "inline-block"
+
+    // Declaring cubes
+    declareCubes() {
+        for (this.n = 0; this.n <= this.totalCharWordChoiced; this.n++) {
+            this.letterArray[this.n] = document.querySelector ("#letter"+this.n)
+        }
     }
-}
 
+    // Create function to insert the cubes
+    insertCubes() {
+        this.containerLetter = document.querySelector ("#container-letter-id")
+        this.count = 1
 
-function tipLetter() {
-    alert (word)
-    letterJ.focus()
-}
-
-
-function insertLetter() {
-    letterJ.focus()
-
-    if (letterJ.value == "") {
-        alert ('Type a letter')
-    } else {
-
-        if (playing) {
-            let obj
-            let letterTmp
-            let letter
-            let searchLetter
-
-            letter = letterJ.value
-            letterJ.value = ""
-            searchLetter = word.match(letter)
-            gotItRight = false
-
-            while (searchLetter != null) {
-                letterTmp = word.search(letter)
-                obj = document.querySelector("letter" + letterTmp).value = letter
-                word = word.replace(letter, '0')
-                hits++
-                searchLetter = word.match(letter)
-                gotItRight = true
-            }
-
-            if (!gotItRight) {
-                document.querySelector('#container-hangman-letterTyped-id').innerHTML += letter.toUpperCase()
-                errors++
-
-                if (errors < 7) {
-                    drawing[errors].style.display = "block"
-                } else {
-                    document.querySelector ("#head-img-id").src = "./images/head-end.png"
-                    document.querySelector ("#container-hangman-messenger-id").innerHTML = `You lose`
-                    playing = false
-                }
-
-            }
-
-            if (hits == heightWord) {
-                document.querySelector ("#container-hangman-messenger-id").innerHTML = ``
-                document.querySelector ("#container-hangman-messenger-id").innerHTML = `You win. Congratulations!`
-                playing = false
-            }
-
+        while (this.count <= this.totalCharWordChoiced) {
+            console.log(this.wordChoiced)
+            this.count++
         }
 
+        this.widthCube = this.letterArray.length-1
+        this.count2 = 1 
+        while (this.count2 <= this.widthCube) {
+            this.letterArray[this.count2].style.display = "inline-block"        
+            this.count2++
+        }
     }
 
+    // Tip on alert
+    tipValue() {
+        alert(this.wordChoiced)
+    }
 }
 
-
-window.addEventListener("load", startGame)
+let hangman = new Hangman()
