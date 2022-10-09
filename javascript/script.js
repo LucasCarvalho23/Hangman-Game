790e5e7d9c2523f81eb2daab8d03a0609f86acce
LucasCarvalho23 <lucasilva1990@hotmail.com>
2022-10-08 22:43:55 -0300

    update JS

--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,11 @@ class Hangman {
         this.letterJ = document.querySelector("#letterJ-id")
         this.gameOpen = false 
         this.arrayFinal = []
+        this.drawing = []
+        this.hit = 0
+        
+        this.declarationDrawing()
+        this.initialPosition()
 
         this.buttonStartId = document.querySelector ("#button-start-id")
         this.insertLetterButton = document.querySelector ("#insertLetter-button-id")
@@ -20,6 +25,25 @@ class Hangman {
         this.insertLetterButton.addEventListener ("click", () => this.insertValues())
         this.tipLetterButton.addEventListener("click",() => this.tipValue())
         
+    }
+
+
+    declarationDrawing() {
+        this.drawing[1] = document.querySelector ("#head-img-id")
+        this.drawing[2] = document.querySelector ("#body-img-id")
+        this.drawing[3] = document.querySelector ("#right-arm-img-id")
+        this.drawing[4] = document.querySelector ("#left-arm-img-id")
+        this.drawing[5] = document.querySelector ("#right-leg-img-id")
+        this.drawing[6] = document.querySelector ("#left-leg-img-id")
+    }
+
+    initialPosition() {
+        this.drawing[1].style.display = "none"
+        this.drawing[2].style.display = "none"
+        this.drawing[3].style.display = "none"
+        this.drawing[4].style.display = "none"
+        this.drawing[5].style.display = "none"
+        this.drawing[6].style.display = "none"
     }
 
     startGame() {
@@ -62,8 +86,10 @@ class Hangman {
 
     // Insert Letters
     insertValues() {
-        if (this.gameOpen == false || this.letterJ.value.length == 0) {
+        if (this.gameOpen == false) {
             alert ("You should start the game.")
+        } else if (this.letterJ.value.length == 0) {
+            alert ("You should typed a letter")
         } else {
             this.arrayFinal = this.wordChoiced.split('')
             this.receiveValues()
@@ -82,9 +108,13 @@ class Hangman {
 
     // Compare values and print
     compareValues() {
+        
+        this.count3 = 0
+
         for (this.textCompare of this.arrayFinal) {
             if (this.letterJ.value == this.textCompare || this.letterJ.value.toLowerCase() == this.textCompare) {
                 console.log (this.letterJ.value)
+                this.count3++
             } else {
                 console.log ('wrong')
                 this.letterJ.focus()
@@ -92,6 +122,17 @@ class Hangman {
         }
 
         this.letterJ.value = ""
+
+        if (this.hit < 7 && this.count3 == 0) {
+            this.hit++
+            this.drawing[this.hit].style.display = "inline-block"
+        } 
+        
+        if (this.hit == 7) {
+            console.log ("End Game")
+            this.gameOpen = false 
+        }
+
     }
 
 
