57f4c894eab716e28c6654f2639d220ee8a69b24
LucasCarvalho23 <lucasilva1990@hotmail.com>
2022-10-08 21:54:45 -0300

    update js

--- javascript/script.js
+++ javascript/script.js
@@ -8,24 +8,32 @@ class Hangman {
         this.totalWords = this.library.length
         this.randomNumber = Math.round(Math.random() * this.totalWords)
         this.letterArray = []
+        this.letterJ = document.querySelector("#letterJ-id")
+        this.gameOpen = false 
+        this.arrayFinal = []
 
         this.buttonStartId = document.querySelector ("#button-start-id")
+        this.insertLetterButton = document.querySelector ("#insertLetter-button-id")
         this.tipLetterButton = document.querySelector ("#tipLetter-button-id")
 
         this.buttonStartId.addEventListener("click",() => this.startGame())
-
-        // Tip
+        this.insertLetterButton.addEventListener ("click", () => this.insertValues())
         this.tipLetterButton.addEventListener("click",() => this.tipValue())
         
     }
 
     startGame() {
+        this.gameOpen = true 
         this.wordChoiced = this.library[this.randomNumber]
         this.totalCharWordChoiced = this.library[this.randomNumber].length
 
         this.declareCubes()
         this.insertCubes()
+
+        this.letterJ.focus()
         
+        console.log (this.letterArray[0].value)
+
     }
 
     // Declaring cubes
@@ -52,6 +60,42 @@ class Hangman {
             this.count2++
         }
     }
+
+
+    // Insert Letters
+    insertValues() {
+        if (this.gameOpen == false || this.letterJ.value.length == 0) {
+            alert ("You should start the game.")
+        } else {
+            this.arrayFinal = this.wordChoiced.split('')
+            this.receiveValues()
+            this.compareValues()
+        }
+    }
+
+
+    // input of the word receive the values the ArrayFinal
+    receiveValues() {
+        for (this.textCompare of this.arrayFinal) {
+            this.letterArray[this.textCompare] = this.textCompare
+        }
+    }
+
+
+    // Compare values and print
+    compareValues() {
+        for (this.textCompare of this.arrayFinal) {
+            if (this.letterJ.value == this.textCompare || this.letterJ.value.toLowerCase() == this.textCompare) {
+                console.log (this.letterJ.value)
+            } else {
+                console.log ('wrong')
+                this.letterJ.focus()
+            }    
+        }
+
+        this.letterJ.value = ""
+    }
+
 
     // Tip on alert
     tipValue() {
